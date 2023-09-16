1f033b35a9dca44a07f3ed87ae18265c8ecb33f3
Alison Souza <alison-cruz@hotmail.com>
2023-09-16 20:13:40 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <>
+      <p>Hello World!!</p>
+      <Button>Continuar</Button>
+    </>
+  );
+};
+
+export default App;
